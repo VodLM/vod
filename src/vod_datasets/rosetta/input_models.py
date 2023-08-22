eda92d224d32ce44fb74acba625adbdeafe53d9c
Andreas Geert Motzfeldt <andreas@motzfeldt.dk>
2023-08-22 17:08:20 +0200

    clean code

--- src/vod_datasets/rosetta/input_models.py
+++ src/vod_datasets/rosetta/input_models.py
@@ -1,36 +1,52 @@
 import typing
+import uuid
 
 import pydantic
+from vod_datasets.models import QueryModel, SectionModel
 
+QUERY_ID_ALIASES = pydantic.AliasChoices("id", "question_id", "query_id")
+SECTION_ID_ALIASES = pydantic.AliasChoices("id", "section_id", "context_id", "passage_id")
+QUERY_ALIASES = pydantic.AliasChoices("query", "question")
+SECTION_ALIASES = pydantic.AliasChoices("passage", "context", "section", "content", "text")
+ANSWER_ALIASES = pydantic.AliasChoices("answer", "answers", "response")
+ANSWER_KEY_ALIASES = pydantic.AliasChoices("value", "text")
+CHOICES_ALIASES = pydantic.AliasChoices("choices", "options", "candidates")
 
 class WithContextMixin(pydantic.BaseModel):
     """A mixin for models with context."""
 
     section: str = pydantic.Field(
         ...,
-        validation_alias=pydantic.AliasChoices("content", "section"),
+        validation_alias=SECTION_ALIASES,
     )
     title: typing.Optional[str] = pydantic.Field(
         None,
     )
 
 
-class AliasQueryModel(pydantic.BaseModel):
+class AliasQueryModel(QueryModel):
     """A query with an alias."""
 
-    id: str = pydantic.Field(
-        ...,
-        validation_alias=pydantic.AliasChoices("id", "query_id"),
-    )
     query: str = pydantic.Field(
         ...,
-        validation_alias=pydantic.AliasChoices("query", "question"),
+        validation_alias=QUERY_ALIASES,
     )
     answer: list[str] = pydantic.Field(
         ...,
-        validation_alias=pydantic.AliasChoices("answer", "answers"),
+        validation_alias=ANSWER_ALIASES,
     )
 
+
+class AliasSectionModel(SectionModel):
+    """A query with an alias."""
+    section: str = pydantic.Field(
+        ...,
+        validation_alias=SECTION_ALIASES,
+    )
+    title: list[str] = pydantic.Field(
+        default=None,
+        validation_alias=pydantic.AliasChoices("title"),
+    )
 
 class AliasQueryWithContextModel(AliasQueryModel, WithContextMixin):
     """A query with context and aliases."""
@@ -39,51 +55,54 @@ class AliasQueryWithContextModel(AliasQueryModel, WithContextMixin):
 class MultipleChoiceQueryModel(pydantic.BaseModel):
     """A multiple-choice query."""
 
-    id: str
+    id: str = pydantic.Field(
+        default=uuid.uuid4().hex,
+        description="The unique identifier for the query.",
+        validation_alias=QUERY_ID_ALIASES,
+    )
     query: str = pydantic.Field(
         ...,
-        validation_alias=pydantic.AliasChoices("query", "question"),
+        validation_alias=QUERY_ALIASES,
     )
-    choices: list[str]
+    choices: list[str] = pydantic.Field(
+        ...,
+        validation_alias=CHOICES_ALIASES,
+    )
     answer: int
 
 
 class AnswerDict(pydantic.BaseModel):
     """Models answer dictionaries (e.g., SQuaD)."""
-
-    answer_start: list[int]
-    text: list[str] = pydantic.Field(
-        ...,
-        validation_alias=pydantic.AliasChoices("text", "value"),
+    
+    text: str | list[str] = pydantic.Field(
+        None,
+        validation_alias=ANSWER_KEY_ALIASES,
     )
 
-    @pydantic.field_validator("answer_start", mode="before")
-    def _validate_answer_start(cls, answer_start: list[int]) -> list[int]:
-        if not isinstance(answer_start, list):
-            return [answer_start]
-
-        return answer_start
-
-    @pydantic.field_validator("text", mode="before")
-    def _validate_text(cls, text: list[str]) -> list[str]:
-        if not isinstance(text, list):
-            return [text]
-
-        return text
+    @pydantic.field_validator("text")
+    def validate_answer_value(cls, value: str | list[str]) -> list[str]:
+        """Validate the dataset name or path."""
+        if isinstance(value, str):
+            return [value]
+        return value
 
 
 class AnswerDictQueryModel(pydantic.BaseModel):
     """A query with an answer dictionary."""
 
-    id: str
+    id: str = pydantic.Field(
+        default=uuid.uuid4().hex,
+        validation_alias=QUERY_ID_ALIASES,
+    )
     query: str = pydantic.Field(
         ...,
-        validation_alias=pydantic.AliasChoices("query", "question"),
+        validation_alias=QUERY_ALIASES,
     )
     answer: AnswerDict = pydantic.Field(
         ...,
-        validation_alias=pydantic.AliasChoices("answer", "answers"),
+        validation_alias=ANSWER_ALIASES,
     )
+
 
 
 class AnswerDictQueryWithContextModel(AnswerDictQueryModel, WithContextMixin):
