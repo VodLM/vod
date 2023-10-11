8ecf55b086dd6a2445eaa73246d022ef59c5eb00
Valentin Liévin <vl@raffle.ai>
2023-10-11 11:00:52 +0200

    feat: export module to remote storage

--- src/vod_workflows/callbacks/export_module.py
+++ src/vod_workflows/callbacks/export_module.py
@@ -1,7 +1,9 @@
-import json
+import os
 import pathlib
 import warnings
 
+import fsspec
+import gcsfs
 import lightning as L
 import torch
 import transformers
@@ -11,25 +13,6 @@ from loguru import logger
 from .base import Callback
 
 
-def _export_module(
-    module: torch.nn.Module | transformers.PreTrainedModel,
-    output_dir: pathlib.Path,
-    templates: None | dict[str, str],
-) -> None:
-    """Export a module."""
-    output_dir.mkdir(parents=True, exist_ok=True)
-    if templates is not None:
-        (output_dir / "templates.json").write_text(json.dumps(templates, indent=2))
-
-    # Handle `transformers.PreTrainedModel`
-    if isinstance(module, transformers.PreTrainedModel):
-        logger.info(f"Exporting module `{type(module).__name__}` to `{output_dir.absolute()}` (HF pretrained)")
-        module.save_pretrained(output_dir)
-    else:
-        logger.info(f"Exporting module `{type(module).__name__}` to `{output_dir.absolute()}` (torch state dict)")
-        torch.save(module.state_dict(), output_dir / "model.pt")
-
-
 class ExportModule(Callback):
     """Pretty print the first training batch."""
 
@@ -37,22 +20,22 @@ class ExportModule(Callback):
         self,
         *,
         output_dir: str | pathlib.Path = "exported-module",
+        include_files: None | list[str] = None,  # Include additional files in the export
+        upload_path: None | str = None,  # Upload the exported module to a remote location
         only_submodule: None | str = None,
         submodules: None | list[str],
-        templates: None | dict[str, str],
         on_fit_end: bool = True,  # at the end the whole training routine
         on_train_end: bool = False,  # at the end of each training period
     ) -> None:
         super().__init__()
         self.output_dir = pathlib.Path(output_dir).expanduser()
+        self.include_files = include_files or []
+        self.upload_path = upload_path
         self.only_submodule = only_submodule
         if only_submodule is not None and submodules is not None:
             submodules = []
             warnings.warn("Both `only_submodule` and `submodules` are set. Ignoring `submodules`.")  # noqa: B028
         self.submodules = submodules
-        if templates is not None:
-            templates = {k: str(v) for k, v in templates.items()}
-        self.templates = templates
 
         # triggers
         self._on_fit_end = on_fit_end
@@ -74,17 +57,88 @@ class ExportModule(Callback):
         if fabric_wrappers.is_wrapped(module):
             module = module.module
 
+        # Export the module
         if self.only_submodule is not None:
             submodule_ = getattr(module, self.only_submodule, None)
             if submodule_ is None:
                 raise ValueError(f"Submodule `{self.only_submodule}` not found in `{type(module).__name__}`")
-            _export_module(submodule_, self.output_dir, self.templates)
+            _export_module(submodule_, self.output_dir, self.include_files)
         elif self.submodules is None:
-            _export_module(module, self.output_dir, self.templates)
+            _export_module(module, self.output_dir, self.include_files)
         else:
             for submodule in self.submodules:
                 submodule_ = getattr(module, submodule, None)
                 if submodule_ is None:
                     logger.debug(f"Submodule `{submodule}` not found in `{type(module).__name__}`")
                     continue
-                _export_module(submodule_, self.output_dir / submodule, self.templates)
+                _export_module(submodule_, self.output_dir / submodule, self.include_files)
+
+        # Upload the module
+        if self.upload_path is not None:
+            _upload_dir(self.output_dir, self.upload_path)
+
+
+def _export_module(
+    module: torch.nn.Module | transformers.PreTrainedModel,
+    output_dir: pathlib.Path,
+    include_files: list[str],
+) -> None:
+    """Export a module."""
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Copy the extra files
+    for f in include_files:
+        logger.info(f"Copying `{f}` to `{output_dir.absolute()}`")
+        if not pathlib.Path(f).exists():
+            warnings.warn(f"File `{f}` does not exist", stacklevel=2)
+        if pathlib.Path(f).is_dir():
+            warnings.warn(f"Cannot copy directory `{f}`. Only files are supported", stacklevel=2)
+        if pathlib.Path(f).is_symlink():
+            warnings.warn(f"Cannot copy symlink `{f}`. Only files are supported", stacklevel=2)
+        os.system(f"cp {f} {output_dir.absolute()}")
+
+    # Handle `transformers.PreTrainedModel`
+    if isinstance(module, transformers.PreTrainedModel):
+        logger.info(f"Exporting module `{type(module).__name__}` to `{output_dir.absolute()}` (HF pretrained)")
+        module.save_pretrained(output_dir)
+    else:
+        logger.info(f"Exporting module `{type(module).__name__}` to `{output_dir.absolute()}` (torch state dict)")
+        torch.save(module.state_dict(), output_dir / "model.pt")
+
+
+def init_gcloud_filesystem() -> fsspec.AbstractFileSystem:
+    """Initialize a GCS filesystem."""
+    try:
+        token = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+    except KeyError as exc:
+        raise RuntimeError("Missing `GOOGLE_APPLICATION_CREDENTIALS` environment variables. ") from exc
+    try:
+        project = os.environ["GCLOUD_PROJECT_ID"]
+    except KeyError as exc:
+        raise RuntimeError("Missing `GCLOUD_PROJECT_ID` environment variables. ") from exc
+    return gcsfs.GCSFileSystem(token=token, project=project)
+
+
+FILESYSTEMS = {
+    "gs": init_gcloud_filesystem,
+}
+
+
+def _upload_dir(local_dir: pathlib.Path, remote_path: str) -> None:
+    """Upload a directory to a remote path."""
+    if "://" not in remote_path:
+        raise ValueError(f"Invalid remote path `{remote_path}`. Must be of the form `protocol://path`")
+    protocol, rpath = remote_path.split("://", 1)
+    if protocol not in FILESYSTEMS:
+        raise ValueError(f"Invalid protocol `{protocol}`. Must be one of {list(FILESYSTEMS.keys())}")
+
+    # Initialize the filesystem
+    fs = FILESYSTEMS[protocol]()
+
+    # Upload the files
+    logger.info(f"Uploading `{local_dir}` to `{remote_path}`")
+    fs.put(
+        str(local_dir),
+        rpath,
+        recursive=True,
+    )
