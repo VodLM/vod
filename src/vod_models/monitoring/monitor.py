fix: DDP - sync gradients by registering ModelOutput as pytree + minor fixes

src/vod_models/monitoring/monitor.py
@@ -45,6 +45,7 @@ class Monitor(abc.ABC, torch.nn.Module):
     def synchronize(self) -> None:
         """Synchronize aggregators between process."""
         if torch.distributed.is_initialized():
+            torch.distributed.barrier()
             for agg in self.aggregators.values():
                 agg.all_reduce()
 
@@ -83,11 +84,11 @@ class RetrievalMonitor(Monitor):
     def update(
         self,
         batch: vt.RealmBatch | typ.Mapping[str, typ.Any],
-        model_output: vt.ModelOutput | typ.Mapping[str, typ.Any],
+        model_output: vt.RealmOutput | typ.Mapping[str, typ.Any],
     ) -> None:
         """Compute metrics and update the aggregators."""
         batch = vt.RealmBatch.cast(batch)
-        model_output = vt.ModelOutput.cast(model_output)
+        model_output = vt.RealmOutput.cast(model_output)
 
         # Rank the relevances and scores by decreasing score value
         ranked_relevances, ranked_scores, n_positives = prepare_for_metric_computation(

src/vod_models/vod_systems/base.py
@@ -55,7 +55,7 @@ class VodSystem(torch.nn.Module):
         self,
         batch: typ.Mapping[str, torch.Tensor],
         **kws: typ.Any,
-    ) -> vt.ModelOutput:  # noqa: ARG002
+    ) -> vt.RealmOutput:  # noqa: ARG002
         """Run a forward pass and compute the gradients."""
         ...
 

src/vod_ops/callbacks/pprint_model_stats.py
@@ -1,6 +1,7 @@
 import lightning as L
 import torch
 from lightning.fabric import wrappers as fabric_wrappers
+from lightning_utilities.core.rank_zero import rank_zero_only
 from vod_tools import pretty
 
 from .base import Callback
@@ -9,8 +10,8 @@ from .base import Callback
 class PprintModelStats(Callback):
     """Pretty print the model statistics."""
 
+    @rank_zero_only
     def on_after_setup(self, *, fabric: L.Fabric, module: torch.nn.Module) -> None:  # noqa: D102
-        if fabric.is_global_zero:
-            if isinstance(module, fabric_wrappers._FabricModule):
-                module = module.module
-            pretty.pprint_parameters_stats(module, header=f"{type(module).__name__} (rank={fabric.global_rank})")
+        if isinstance(module, fabric_wrappers._FabricModule):
+            module = module.module
+        pretty.pprint_parameters_stats(module, header=f"{type(module).__name__} (rank={fabric.global_rank})")